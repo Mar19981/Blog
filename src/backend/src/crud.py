9bd2cbffe3c8e46f6fc8fc4c7fdb58c5221e44db
Mateusz Adamczyk <mateuszadamczyk@poczta.fm>
2022-11-24 23:51:17 +0100

    Update DB Model.

--- src/backend/src/crud.py
+++ src/backend/src/crud.py
@@ -1,94 +1,95 @@
 #region IMPORTS
 import models
+import schemas
 # import schemas
 from sqlalchemy.orm import Session
 #endregion
 
 #region USER
-def get_users(db: Session):
+def get_users(db: Session) -> list[schemas.User]:
     return db.query(models.User).all()
 
-def get_users_active(db: Session):
+def get_users_active(db: Session) -> list[schemas.User]:
     return db.query(models.User).filter(models.User.is_active == True).all()
 
-def get_users_inactive(db: Session):
+def get_users_inactive(db: Session) -> list[schemas.User]:
     return db.query(models.User).filter(models.User.is_active == False).all()
 
-def get_user_by_id(db: Session, sysuser: int):
+def get_user_by_id(db: Session, sysuser: int) -> schemas.User:
     return db.query(models.User).filter(models.User.sysuser == sysuser).first()
 
-def get_user_by_username(db: Session, username: str):
+def get_user_by_username(db: Session, username: str) -> schemas.User:
     return db.query(models.User).filter(models.User.username == username).first()
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str) -> schemas.User:
     return db.query(models.User).filter(models.User.email == email).first()
 
-# def get_user_newses_by_id(db: Session, sysuser: int):
-#     return db.query(models.News).join(models.User, models.News.create_sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).all()
+def get_user_newses_by_id(db: Session, sysuser: int) -> list[schemas.News]:
+    return db.query(models.News).join(models.User, models.News.create_sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).all()
 
-# def get_user_newses_active_by_id(db: Session, sysuser: int):
-#     return db.query(models.News).join(models.User, models.News.create_sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.News.is_active == True).all()
+def get_user_newses_active_by_id(db: Session, sysuser: int) -> list[schemas.News]:
+    return db.query(models.News).join(models.User, models.News.create_sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.News.is_active == True).all()
 
-# def get_user_newses_inactive_by_id(db: Session, sysuser: int):
-#     return db.query(models.News).join(models.User, models.News.create_sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.News.is_active == False).all()
+def get_user_newses_inactive_by_id(db: Session, sysuser: int) -> list[schemas.News]:
+    return db.query(models.News).join(models.User, models.News.create_sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.News.is_active == False).all()
 
-# def get_user_comments_by_id(db: Session, sysuser: int):
-#     return db.query(models.Comment).join(models.User, models.Comment.sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).all()
+def get_user_comments_by_id(db: Session, sysuser: int) -> list[schemas.Comment]:
+    return db.query(models.Comment).join(models.User, models.Comment.sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).all()
 
-# def get_user_comments_active_by_id(db: Session, sysuser: int):
-#     return db.query(models.Comment).join(models.User, models.Comment.sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.Comment.is_active == True).all()
+def get_user_comments_active_by_id(db: Session, sysuser: int) -> list[schemas.Comment]:
+    return db.query(models.Comment).join(models.User, models.Comment.sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.Comment.is_active == True).all()
 
-# def get_user_comments_inactive_by_id(db: Session, sysuser: int):
-#     return db.query(models.Comment).join(models.User, models.Comment.sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.Comment.is_active == False).all()
+def get_user_comments_inactive_by_id(db: Session, sysuser: int) -> list[schemas.Comment]:
+    return db.query(models.Comment).join(models.User, models.Comment.sysuser == models.User.sysuser).filter(models.User.sysuser == sysuser).filter(models.Comment.is_active == False).all()
 # #endregion
 
 # #region NEWS
-def get_newses(db: Session):
+def get_newses(db: Session) -> list[schemas.News]:
     return db.query(models.News).all()
 
-def get_newses_active(db: Session):
+def get_newses_active(db: Session) -> list[schemas.News]:
     return db.query(models.News).filter(models.News.is_active == True).all()
 
-def get_newses_inactive(db: Session):
+def get_newses_inactive(db: Session) -> list[schemas.News]:
     return db.query(models.News).filter(models.News.is_active == False).all()
 
-def get_news_by_id(db: Session, sysnews: int):
+def get_news_by_id(db: Session, sysnews: int) -> schemas.News:
     return db.query(models.News).filter(models.News.sysnews == sysnews).first()
 
-# def get_news_comments_by_id(db: Session, sysnews: int):
-#     return db.query(models.Comment).join(models.News, models.Comment.sysnews == models.News.sysnews).filter(models.News.sysnews == sysnews).all()
+def get_news_comments_by_id(db: Session, sysnews: int) -> list[schemas.Comment]:
+    return db.query(models.Comment).join(models.News, models.Comment.sysnews == models.News.sysnews).filter(models.News.sysnews == sysnews).all()
 
-# def get_news_comments_active_by_id(db: Session, sysnews: int):
-#     return db.query(models.Comment).join(models.News, models.Comment.sysnews == models.News.sysnews).filter(models.News.sysnews == sysnews).filter(models.Comment.is_active == True).all()
+def get_news_comments_active_by_id(db: Session, sysnews: int) -> list[schemas.Comment]:
+    return db.query(models.Comment).join(models.News, models.Comment.sysnews == models.News.sysnews).filter(models.News.sysnews == sysnews).filter(models.Comment.is_active == True).all()
 
-# def get_news_comments_inactive_by_id(db: Session, sysnews: int):
-#     return db.query(models.Comment).join(models.News, models.Comment.sysnews == models.News.sysnews).filter(models.News.sysnews == sysnews).filter(models.Comment.is_active == False).all()
+def get_news_comments_inactive_by_id(db: Session, sysnews: int) -> list[schemas.Comment]:
+    return db.query(models.Comment).join(models.News, models.Comment.sysnews == models.News.sysnews).filter(models.News.sysnews == sysnews).filter(models.Comment.is_active == False).all()
 
-# def get_news_create_user_by_id(db: Session, sysnews: int):
-#     return db.query(models.User).join(models.News, models.User.sysuser == models.News.create_sysuser).filter(models.News.sysnews == sysnews).first()
+def get_news_create_user_by_id(db: Session, sysnews: int) -> schemas.User:
+    return db.query(models.User).join(models.News, models.User.sysuser == models.News.create_sysuser).filter(models.News.sysnews == sysnews).first()
 
-# def get_news_update_user_by_id(db: Session, sysnews: int):
-#     return db.query(models.User).join(models.News, models.User.sysuser == models.News.update_sysuser).filter(models.News.sysnews == sysnews).first()
+def get_news_update_user_by_id(db: Session, sysnews: int) -> schemas.User:
+    return db.query(models.User).join(models.News, models.User.sysuser == models.News.update_sysuser).filter(models.News.sysnews == sysnews).first()
 # #endregion
 
 # #region COMMENT
-def get_comments(db: Session):
+def get_comments(db: Session) -> list[schemas.Comment]:
     return db.query(models.Comment).all()
 
-def get_comments_active(db: Session):
+def get_comments_active(db: Session) -> list[schemas.Comment]:
     return db.query(models.Comment).filter(models.Comment.is_active == True).all()
 
-def get_comments_inactive(db: Session):
+def get_comments_inactive(db: Session) -> list[schemas.Comment]:
     return db.query(models.Comment).filter(models.Comment.is_active == False).all()
 
-def get_comment_by_id(db: Session, syscomment: int):
+def get_comment_by_id(db: Session, syscomment: int) -> schemas.Comment:
     return db.query(models.Comment).filter(models.Comment.syscomment == syscomment).first()
 
-# def get_comment_user_by_id(db: Session, syscomment: int):
-#     return db.query(models.User).join(models.Comment, models.User.sysuser == models.Comment.sysuser).filter(models.Comment.syscomment == syscomment).first()
+def get_comment_user_by_id(db: Session, syscomment: int) -> schemas.User:
+    return db.query(models.User).join(models.Comment, models.User.sysuser == models.Comment.sysuser).filter(models.Comment.syscomment == syscomment).first()
 
-# def get_comment_news_by_id(db: Session, syscomment: int):
-#     return db.query(models.News).join(models.Comment, models.News.sysnews == models.Comment.sysnews).filter(models.Comment.syscomment == syscomment).first()
+def get_comment_news_by_id(db: Session, syscomment: int) -> schemas.News:
+    return db.query(models.News).join(models.Comment, models.News.sysnews == models.Comment.sysnews).filter(models.Comment.syscomment == syscomment).first()
 #endregion
 
 # def create_user(db: Session, user: schemas.UserCreate):
--- src/backend/src/main.py
+++ src/backend/src/main.py
@@ -1,4 +1,5 @@
 #region IMPORTS
+import json
 import crud
 import models
 import schemas
@@ -20,203 +21,260 @@ def get_db():
         db.close()
 #endregion
 
-#region OPERATIONS
-@app.get("/")
-def read_root():
-    return {"Hello": "WORLD !!!"}
-#endregion
-
 #region USER
 @app.get("/users", response_model = list[schemas.User])
 def read_users(db: Session = Depends(get_db)):
-    users = crud.get_users(db)
-    if users is None:
+    db_users = crud.get_users(db)
+    if db_users == []:
         raise HTTPException(status_code = 404, detail = "Users not found!")
-    return users
+    return db_users
 
 @app.get("/users/active", response_model = list[schemas.User])
 def read_users_active(db: Session = Depends(get_db)):
-    users = crud.get_users_active(db)
-    if users is None:
+    db_users = crud.get_users_active(db)
+    if db_users == []:
         raise HTTPException(status_code = 404, detail = "Active Users not found!")
-    return users
+    return db_users
 
 @app.get("/users/inactive", response_model = list[schemas.User])
 def read_users_inactive(db: Session = Depends(get_db)):
-    users = crud.get_users_inactive(db)
-    if users is None:
+    db_users = crud.get_users_inactive(db)
+    if db_users == []:
         raise HTTPException(status_code = 404, detail = "Inactive Users not found!")
-    return users
+    return db_users
 
 @app.get("/user/id={sysuser}", response_model = schemas.User)
 def read_user_by_id(sysuser: int, db: Session = Depends(get_db)):
-    user = crud.get_user_by_id(db, sysuser = sysuser)
-    if user is None:
+    db_user = crud.get_user_by_id(db, sysuser)
+    if db_user is None:
         raise HTTPException(status_code = 404, detail = "User not found!")
-    return user
+    return db_user
+
+@app.delete("/user/id={sysuser}", response_model = bool)
+def delete_user_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, sysuser)
+    if db_user is None:
+        raise HTTPException(status_code = 404, detail = "User not found!")
+    db_user.is_active = False
+    db.commit()
+    return True
 
 @app.get("/user/username={username}", response_model = schemas.User)
 def read_user_by_username(username: str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_username(db, username = username)
-    if user is None:
+    db_user = crud.get_user_by_username(db, username)
+    if db_user is None:
         raise HTTPException(status_code = 404, detail = "User not found!")
-    return user
+    return db_user
 
 @app.get("/user/email={email}", response_model = schemas.User)
 def read_user_by_email(email: str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_email(db, email = email)
-    if user is None:
+    db_user = crud.get_user_by_email(db, email)
+    if db_user is None:
         raise HTTPException(status_code = 404, detail = "User not found!")
-    return user
+    return db_user
 
-# @app.get("/user/id={sysuser}/newses", response_model = list[schemas.News])
-# def read_user_newses_by_id(sysuser: int, db: Session = Depends(get_db)):
-#     newses = crud.get_user_newses_by_id(db, sysuser = sysuser)
-#     if newses is None:
-#         raise HTTPException(status_code = 404, detail = "User Newses not found!")
-#     return newses
+@app.get("/user/id={sysuser}/newses", response_model = list[schemas.News])
+def read_user_newses_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_newses = crud.get_user_newses_by_id(db, sysuser)
+    if db_newses == []:
+        raise HTTPException(status_code = 404, detail = "User Newses not found!")
+    return db_newses
 
-# @app.get("/user/id={sysuser}/newses/active", response_model = list[schemas.News])
-# def read_user_newses_active_by_id(sysuser: int, db: Session = Depends(get_db)):
-#     newses = crud.get_user_newses_active_by_id(db, sysuser = sysuser)
-#     if newses is None:
-#         raise HTTPException(status_code = 404, detail = "User Active Newses not found!")
-#     return newses
+@app.delete("/user/id={sysuser}/newses", response_model = bool)
+def delete_user_newses_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_newses = crud.get_user_newses_active_by_id(db, sysuser)
+    if db_newses == []:
+        raise HTTPException(status_code = 404, detail = "User Newses not found!")
+    for db_news in db_newses:
+        db_comments = crud.get_news_comments_active_by_id(db, db_news.sysnews)
+        for db_comment in db_comments:
+            db_comment.is_active = False
+        db_news.is_active = False
+    db.commit()
+    return True
 
-# @app.get("/user/id={sysuser}/newses/inactive", response_model = list[schemas.News])
-# def read_user_newses_inactive_by_id(sysuser: int, db: Session = Depends(get_db)):
-#     newses = crud.get_user_newses_inactive_by_id(db, sysuser = sysuser)
-#     if newses is None:
-#         raise HTTPException(status_code = 404, detail = "User Inactive Newses not found!")
-#     return newses
+@app.get("/user/id={sysuser}/newses/active", response_model = list[schemas.News])
+def read_user_newses_active_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_newses = crud.get_user_newses_active_by_id(db, sysuser)
+    if db_newses == []:
+        raise HTTPException(status_code = 404, detail = "User Active Newses not found!")
+    return db_newses
 
-# @app.get("/user/id={sysuser}/comments", response_model = list[schemas.Comment])
-# def read_user_comments_by_id(sysuser: int, db: Session = Depends(get_db)):
-#     comments = crud.get_user_comments_by_id(db, sysuser = sysuser)
-#     if comments is None:
-#         raise HTTPException(status_code = 404, detail = "User Comments not found!")
-#     return comments
+@app.get("/user/id={sysuser}/newses/inactive", response_model = list[schemas.News])
+def read_user_newses_inactive_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_newses = crud.get_user_newses_inactive_by_id(db, sysuser)
+    if db_newses == []:
+        raise HTTPException(status_code = 404, detail = "User Inactive Newses not found!")
+    return db_newses
 
-# @app.get("/user/id={sysuser}/comments/active", response_model = list[schemas.Comment])
-# def read_user_comments_active_by_id(sysuser: int, db: Session = Depends(get_db)):
-#     comments = crud.get_user_comments_active_by_id(db, sysuser = sysuser)
-#     if comments is None:
-#         raise HTTPException(status_code = 404, detail = "User Active Comments not found!")
-#     return comments
+@app.get("/user/id={sysuser}/comments", response_model = list[schemas.Comment])
+def read_user_comments_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_user_comments_by_id(db, sysuser)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "User Comments not found!")
+    return db_comments
 
-# @app.get("/user/id={sysuser}/comments/inactive", response_model = list[schemas.Comment])
-# def read_user_comments_inactive_by_id(sysuser: int, db: Session = Depends(get_db)):
-#     comments = crud.get_user_comments_inactive_by_id(db, sysuser = sysuser)
-#     if comments is None:
-#         raise HTTPException(status_code = 404, detail = "User Inactive Comments not found!")
-#     return comments
+@app.delete("/user/id={sysuser}/comments", response_model = bool)
+def delete_user_comments_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_user_comments_active_by_id(db, sysuser)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "User Comments not found!")
+    for db_comment in db_comments:
+        db_comment.is_active = False
+    db.commit()
+    return True
+
+@app.get("/user/id={sysuser}/comments/active", response_model = list[schemas.Comment])
+def read_user_comments_active_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_user_comments_active_by_id(db, sysuser)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "User Active Comments not found!")
+    return db_comments
+
+@app.get("/user/id={sysuser}/comments/inactive", response_model = list[schemas.Comment])
+def read_user_comments_inactive_by_id(sysuser: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_user_comments_inactive_by_id(db, sysuser)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "User Inactive Comments not found!")
+    return db_comments
 #endregion
 
 #region NEWS
 @app.get("/newses", response_model = list[schemas.News])
 def read_newses(db: Session = Depends(get_db)):
-    newses = crud.get_newses(db)
-    if newses is None:
+    db_newses = crud.get_newses(db)
+    if db_newses == []:
         raise HTTPException(status_code = 404, detail = "Newses not found!")
-    return newses
+    return db_newses
 
 @app.get("/newses/active", response_model = list[schemas.News])
 def read_newses_active(db: Session = Depends(get_db)):
-    newses = crud.get_newses_active(db)
-    if newses is None:
+    db_newses = crud.get_newses_active(db)
+    if db_newses == []:
         raise HTTPException(status_code = 404, detail = "Active Newses not found!")
-    return newses
+    return db_newses
 
 @app.get("/newses/inactive", response_model = list[schemas.News])
 def read_newses_inactive(db: Session = Depends(get_db)):
-    newses = crud.get_newses_inactive(db)
-    if newses is None:
+    db_newses = crud.get_newses_inactive(db)
+    if db_newses == []:
         raise HTTPException(status_code = 404, detail = "Inactive Newses not found!")
-    return newses
+    return db_newses
 
 @app.get("/news/id={sysnews}", response_model = schemas.News)
-def read_user_by_id(sysnews: int, db: Session = Depends(get_db)):
-    news = crud.get_news_by_id(db, sysnews = sysnews)
-    if news is None:
+def read_news_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_news = crud.get_news_by_id(db, sysnews)
+    if db_news is None:
         raise HTTPException(status_code = 404, detail = "News not found!")
-    return news
+    return db_news
 
-# @app.get("/news/id={sysnews}/comments", response_model = list[schemas.Comment])
-# def read_news_comments_by_id(sysnews: int, db: Session = Depends(get_db)):
-#     comments = crud.get_news_comments_by_id(db, sysnews = sysnews)
-#     if comments is None:
-#         raise HTTPException(status_code = 404, detail = "News Comments not found!")
-#     return comments
+@app.delete("/news/id={sysnews}", response_model = bool)
+def delete_news_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_news = crud.get_news_by_id(db, sysnews)
+    if db_news is None:
+        raise HTTPException(status_code = 404, detail = "News not found!")
+    db_comments = crud.get_news_comments_active_by_id(db, db_news.sysnews)
+    for db_comment in db_comments:
+        db_comment.is_active = False
+    db_news.is_active = False
+    db.commit()
+    return True
 
-# @app.get("/news/id={sysnews}/comments/active", response_model = list[schemas.Comment])
-# def read_news_comments_active_by_id(sysnews: int, db: Session = Depends(get_db)):
-#     comments = crud.get_news_comments_active_by_id(db, sysnews = sysnews)
-#     if comments is None:
-#         raise HTTPException(status_code = 404, detail = "News Active Comments not found!")
-#     return comments
+@app.get("/news/id={sysnews}/comments", response_model = list[schemas.Comment])
+def read_news_comments_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_news_comments_by_id(db, sysnews)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "News Comments not found!")
+    return db_comments
 
-# @app.get("/news/id={sysnews}/comments/inactive", response_model = list[schemas.Comment])
-# def read_news_comments_inactive_by_id(sysnews: int, db: Session = Depends(get_db)):
-#     comments = crud.get_news_comments_inactive_by_id(db, sysnews = sysnews)
-#     if comments is None:
-#         raise HTTPException(status_code = 404, detail = "News Inactive Comments not found!")
-#     return comments
+@app.delete("/news/id={sysnews}/comments", response_model = bool)
+def delete_news_comments_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_news_comments_active_by_id(db, sysnews)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "News Comments not found!")
+    for db_comment in db_comments:
+        db_comment.is_active = False
+    db.commit()
+    return True
 
-# @app.get("/news/id={sysnews}/create_user", response_model = schemas.User)
-# def read_news_create_user_by_id(sysnews: int, db: Session = Depends(get_db)):
-#     user = crud.get_news_create_user_by_id(db, sysnews = sysnews)
-#     if user is None:
-#         raise HTTPException(status_code = 404, detail = "News Create User not found!")
-#     return user
+@app.get("/news/id={sysnews}/comments/active", response_model = list[schemas.Comment])
+def read_news_comments_active_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_news_comments_active_by_id(db, sysnews)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "News Active Comments not found!")
+    return db_comments
 
-# @app.get("/news/id={sysnews}/update_user", response_model = schemas.User)
-# def read_news_update_user_by_id(sysnews: int, db: Session = Depends(get_db)):
-#     user = crud.get_news_update_user_by_id(db, sysnews = sysnews)
-#     if user is None:
-#         raise HTTPException(status_code = 404, detail = "News Update User not found!")
-#     return user
+@app.get("/news/id={sysnews}/comments/inactive", response_model = list[schemas.Comment])
+def read_news_comments_inactive_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_comments = crud.get_news_comments_inactive_by_id(db, sysnews)
+    if db_comments == []:
+        raise HTTPException(status_code = 404, detail = "News Inactive Comments not found!")
+    return db_comments
+
+@app.get("/news/id={sysnews}/create_user", response_model = schemas.User)
+def read_news_create_user_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_user = crud.get_news_create_user_by_id(db, sysnews)
+    if db_user is None:
+        raise HTTPException(status_code = 404, detail = "News Create User not found!")
+    return db_user
+
+@app.get("/news/id={sysnews}/update_user", response_model = schemas.User)
+def read_news_update_user_by_id(sysnews: int, db: Session = Depends(get_db)):
+    db_user = crud.get_news_update_user_by_id(db, sysnews)
+    if db_user is None:
+        raise HTTPException(status_code = 404, detail = "News Update User not found!")
+    return db_user
 #endregion
 
 #region COMMENT
 @app.get("/comments", response_model = list[schemas.Comment])
 def read_comments(db: Session = Depends(get_db)):
-    comments = crud.get_comments(db)
-    if comments is None:
+    db_comments = crud.get_comments(db)
+    if db_comments == []:
         raise HTTPException(status_code = 404, detail = "Comments not found!")
-    return comments
+    return db_comments
 
 @app.get("/comments/active", response_model = list[schemas.Comment])
 def read_comments_active(db: Session = Depends(get_db)):
-    comments = crud.get_comments_active(db)
-    if comments is None:
+    db_comments = crud.get_comments_active(db)
+    if db_comments == []:
         raise HTTPException(status_code = 404, detail = "Active Comments not found!")
-    return comments
+    return db_comments
 
 @app.get("/comments/inactive", response_model = list[schemas.Comment])
 def read_comments_inactive(db: Session = Depends(get_db)):
-    comments = crud.get_comments_inactive(db)
-    if comments is None:
+    db_comments = crud.get_comments_inactive(db)
+    if db_comments == []:
         raise HTTPException(status_code = 404, detail = "Inactive Comments not found!")
-    return comments
+    return db_comments
 
 @app.get("/comment/id={syscomment}", response_model = schemas.Comment)
 def read_comment_by_id(syscomment: int, db: Session = Depends(get_db)):
-    comment = crud.get_comment_by_id(db, syscomment = syscomment)
-    if comment is None:
+    db_comment = crud.get_comment_by_id(db, syscomment)
+    if db_comment is None:
         raise HTTPException(status_code = 404, detail = "Comment not found!")
-    return comment
+    return db_comment
 
-# @app.get("/comment/id={syscomment}/user", response_model = schemas.User)
-# def read_comment_user_by_id(syscomment: int, db: Session = Depends(get_db)):
-#     user = crud.get_comment_user_by_id(db, syscomment = syscomment)
-#     if user is None:
-#         raise HTTPException(status_code = 404, detail = "Comment User not found!")
-#     return user
+@app.delete("/comment/id={syscomment}", response_model = bool)
+def delete_comment_by_id(syscomment: int, db: Session = Depends(get_db)):
+    db_comment = crud.get_comment_by_id(db, syscomment)
+    if db_comment is None:
+        raise HTTPException(status_code = 404, detail = "Comment not found!")
+    db_comment.is_active = False
+    db.commit()
+    return True
 
-# @app.get("/comment/id={syscomment}/news", response_model = schemas.News)
-# def read_comment_news_by_id(syscomment: int, db: Session = Depends(get_db)):
-#     news = crud.get_comment_news_by_id(db, syscomment = syscomment)
-#     if news is None:
-#         raise HTTPException(status_code = 404, detail = "Comment News not found!")
-#     return news
+@app.get("/comment/id={syscomment}/user", response_model = schemas.User)
+def read_comment_user_by_id(syscomment: int, db: Session = Depends(get_db)):
+    db_user = crud.get_comment_user_by_id(db, syscomment)
+    if db_user is None:
+        raise HTTPException(status_code = 404, detail = "Comment User not found!")
+    return db_user
+
+@app.get("/comment/id={syscomment}/news", response_model = schemas.News)
+def read_comment_news_by_id(syscomment: int, db: Session = Depends(get_db)):
+    db_news = crud.get_comment_news_by_id(db, syscomment)
+    if db_news is None:
+        raise HTTPException(status_code = 404, detail = "Comment News not found!")
+    return db_news
 #endregion
